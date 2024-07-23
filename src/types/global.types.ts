3bf693c724fcb070833c4936059da9bd859b2e14
ashokkkwr <ashokkatwal9811@gmail.com>
2024-07-24 05:11:20 +0545

    started working with signup and language context

--- src/types/global.types.ts
+++ src/types/global.types.ts
@@ -7,9 +7,28 @@ This inner Record<string, string> means that this inner object
 can have its own string keys, each of which maps to a string value.
 */
 export type LabelTypes = Record<string, Record<string, string>>
-
+//This defines an enumeration named LanguageEnum with two possible values:
 export enum LanguageEnum {
   en = 'en',
   ne = 'ne',
 }
+//keyof is a TypeScript keyword that creates a union of the keys of an object type.
+/**
+ * keyof typeof LanguageEnum creates a union of the keys of the 
+ * LanguageEnum type:
+'   en' | 'ne'
+ */
 export type language = keyof typeof LanguageEnum
+
+export type LanguageType = {
+//The type of lang is LanguageEnum, which means it can only be 
+//one of the values defined in the LanguageEnum enum.
+    lang:LanguageEnum
+/**
+setLang is a function property of the LanguageType.
+This function takes one parameter lang of type LanguageEnum.
+The function returns void, which means it doesn't return any value. 
+*/
+    setLang:(lang:LanguageEnum)=>void
+}
+export type multiLanguage = Record<string,string>
