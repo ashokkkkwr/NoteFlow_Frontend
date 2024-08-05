2d1b220d006a61897570eafe1570435484d5cbfc
Your Name <you@example.com>
2024-08-05 16:05:32 +0545

    chat backend complete

--- src/ui/common/organism/Posts.tsx
+++ src/ui/common/organism/Posts.tsx
@@ -5,15 +5,16 @@ import Button from '../atoms/Button';
 import useLang from '@hooks/useLang';
 import { RiCornerRightDownFill } from "react-icons/ri";
 import { IoClose } from "react-icons/io5";
+import CommentComponent from './CommentComponent';
 
 
-interface Comment {
+export interface Comment {
   id: string;
   comment: string;
   replies: Comment[];
 }
 
-interface Note {
+export interface Note {
   id: string;
   title: string;
   content: string;
@@ -36,58 +37,58 @@ interface Media {
   path: string;
 }
 
-const CommentComponent: React.FC<{
-  noteId: string;
-  comment: Comment;
-  handleReplySubmit: (e: React.FormEvent<HTMLFormElement>, noteId: string, commentId: string) => void;
-  handleReplyChange: (noteId: string, commentId: string, value: string) => void;
-  visibleReplyForm: { [key: string]: string };
-  replyForm: { [key: string]: { [key: string]: string } };
-  toggleReplyFormVisibility: (noteId: string, commentId: string) => void;
-}> = ({
-  noteId,
-  comment,
-  handleReplySubmit,
-  handleReplyChange,
-  visibleReplyForm,
-  replyForm,
-  toggleReplyFormVisibility,
-}) => (
-  <div key={comment.id} className="ml-10">
-    <div>
+// const CommentComponent: React.FC<{
+//   noteId: string;
+//   comment: Comment;
+//   handleReplySubmit: (e: React.FormEvent<HTMLFormElement>, noteId: string, commentId: string) => void;
+//   handleReplyChange: (noteId: string, commentId: string, value: string) => void;
+//   visibleReplyForm: { [key: string]: string };
+//   replyForm: { [key: string]: { [key: string]: string } };
+//   toggleReplyFormVisibility: (noteId: string, commentId: string) => void;
+// }> = ({
+//   noteId,
+//   comment,
+//   handleReplySubmit,
+//   handleReplyChange,
+//   visibleReplyForm,
+//   replyForm,
+//   toggleReplyFormVisibility,
+// }) => (
+//   <div key={comment.id} className="ml-10">
+//     <div>
 
-    <p className='font-poppins font-bold flex'>{comment.comment}<RiCornerRightDownFill /></p></div>
-    <button onClick={() => toggleReplyFormVisibility(noteId, comment.id)}>
-      <p>{visibleReplyForm[noteId] === comment.id ? <IoClose /> : 'Reply'}</p>
-    </button>
-    {visibleReplyForm[noteId] === comment.id && (
-      <form onSubmit={(e) => handleReplySubmit(e, noteId, comment.id)}>
-        <div className='flex'>
-        <InputField
-          name="comment"
-          type="text"
-          placeholder="Add a reply"
-          onChange={(e) => handleReplyChange(noteId, comment.id, e.target.value)}
-          value={replyForm[noteId]?.[comment.id] || ''}
-        />
-        <Button type="submit" buttonText="Submit" />
-        </div>
-      </form>
-    )}
-    {comment.replies?.map((reply) => (
-      <CommentComponent
-        key={reply.id}
-        noteId={noteId}
-        comment={reply}
-        handleReplySubmit={handleReplySubmit}
-        handleReplyChange={handleReplyChange}
-        visibleReplyForm={visibleReplyForm}
-        replyForm={replyForm}
-        toggleReplyFormVisibility={toggleReplyFormVisibility}
-      />
-    ))}
-  </div>
-);
+//     <p className='font-poppins font-bold flex'>{comment.comment}<RiCornerRightDownFill /></p></div>
+//     <button onClick={() => toggleReplyFormVisibility(noteId, comment.id)}>
+//       <p>{visibleReplyForm[noteId] === comment.id ? <IoClose /> : 'Reply'}</p>
+//     </button>
+//     {visibleReplyForm[noteId] === comment.id && (
+//       <form onSubmit={(e) => handleReplySubmit(e, noteId, comment.id)}>
+//         <div className='flex'>
+//         <InputField
+//           name="comment"
+//           type="text"
+//           placeholder="Add a reply"
+//           onChange={(e) => handleReplyChange(noteId, comment.id, e.target.value)}
+//           value={replyForm[noteId]?.[comment.id] || ''}
+//         />
+//         <Button type="submit" buttonText="Submit" />
+//         </div>
+//       </form>
+//     )}
+//     {comment.replies?.map((reply) => (
+//       <CommentComponent
+//         key={reply.id}
+//         noteId={noteId}
+//         comment={reply}
+//         handleReplySubmit={handleReplySubmit}
+//         handleReplyChange={handleReplyChange}
+//         visibleReplyForm={visibleReplyForm}
+//         replyForm={replyForm}
+//         toggleReplyFormVisibility={toggleReplyFormVisibility}
+//       />
+//     ))}
+//   </div>
+// );
 
 export default function Posts() {
   const { lang } = useLang();
