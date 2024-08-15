ed29de957e46fa3af9d98b987ac6e7457eed46ab
Your Name <you@example.com>
2024-08-15 06:09:43 +0545

    style: made the Users Profile Page Responsive

--- src/ui/common/organism/Profiles.tsx
+++ src/ui/common/organism/Profiles.tsx
@@ -2,8 +2,8 @@ import useLang from '@hooks/useLang'
 import React, { useEffect, useState } from 'react'
 import { FaChevronDown, FaEdit } from 'react-icons/fa' // Import FaEdit
 import axiosInstance from 'services/instance'
-import { useRightSidebar } from '@context/RightSidebarContext';
-import { useSidebar } from '@context/SidebarContext';
+import { useRightSidebar } from '@context/RightSidebarContext'
+import { useSidebar } from '@context/SidebarContext'
 interface User {
   id: string
   createdAt: any
@@ -23,8 +23,8 @@ interface Media {
 }
 export default function Profiles() {
   const [user, setUser] = useState<User | null>(null)
-  const { isRightSidebarOpen } = useRightSidebar();
-  const { isSidebarOpen } = useSidebar();
+  const { isRightSidebarOpen } = useRightSidebar()
+  const { isSidebarOpen } = useSidebar()
 
   const viewUser = async () => {
     try {
@@ -36,9 +36,6 @@ export default function Profiles() {
     }
   }
 
-
-
-
   useEffect(() => {
     viewUser()
   }, [])
@@ -48,48 +45,47 @@ export default function Profiles() {
   return (
     //w-[119vh]
     // <div className={`mt-10 bg-grey max-w-3xl ${isRightSidebarOpen?'hidden':'block'} ${isSidebarOpen?'hidden':'block'} 2xl:block  `}>
-    <div className={`mt-2 bg-white cursor-pointer shadow-lg rounded-lg w-[50vh]  h-[87vh] flex items-center justify-center ${isRightSidebarOpen?'hidden':'block'} ${isSidebarOpen?'hidden':'block'} 2xl:block  2xl:w-[119vh]`}>
-  <div className="flex flex-col items-center">
-    {user?.details.profileImage.map((media) => (
-      <div key={media.id} className="flex justify-center">
-        <img
-          src={`${media.path}`}
-          alt={`Profile ${media.id}`}
-          className="w-96 h-96 rounded-full object-cover"
-        />
-      </div>
-    ))}
+    <div
+      className={`mt-2 bg-white cursor-pointer shadow-lg rounded-lg w-[50vh]  h-[87vh] flex items-center justify-center ${
+        isRightSidebarOpen ? 'hidden' : 'block'
+      } ${isSidebarOpen ? 'hidden' : 'block'} 2xl:block  2xl:w-[119vh]`}
+    >
+      <div className='flex flex-col items-center'>
+        {user?.details.profileImage.map((media) => (
+          <div key={media.id} className='flex justify-center'>
+            <img src={`${media.path}`} alt={`Profile ${media.id}`} className='w-96 h-96 rounded-full object-cover' />
+          </div>
+        ))}
 
-    <div className="mt-2">
-      <div key={user?.id}>
-        <div className="flex mt-10 justify-center">
-          <p className="text-4xl font-bold">{user?.details.first_name}</p>
-          <p className="ml-1 text-4xl font-bold">{user?.details.last_name}</p>
-        </div>
-        <div className="flex mt-5 justify-center">
-          <p className="text-lg">Email:</p>
-          <p className="ml-5 text-lg">{user?.email}</p>
-        </div>
-        <div className="flex mt-1 justify-center">
-          <p className="text-lg">Phone:</p>
-          <p className="ml-3 text-lg">{user?.details.phone_number}</p>
-        </div>
-        <div className="flex mt-1 justify-center">
-          <p className="text-lg">Role:</p>
-          <p className="ml-7 text-lg">{user?.role}</p>
-        </div>
+        <div className=' ml mt-2'>
+          <div className='ml-' key={user?.id}>
+            <div className='flex mt-10'>
+              <p className='text-4xl font-bold'>{user?.details.first_name}</p>
+              <p className='ml-1 text-4xl font-bold'>{user?.details.last_name}</p>
+            </div>
+            <div className='flex mt-5 justify-center'>
+              <p className='text-lg'>Email:</p>
+              <p className='ml-5 text-lg'>{user?.email}</p>
+            </div>
+            <div className='flex mt-1 justify-center'>
+              <p className='text-lg'>Phone:</p>
+              <p className='ml-3 text-lg'>{user?.details.phone_number}</p>
+            </div>
+            <div className='flex mt-1 justify-center'>
+              <p className='text-lg'>Role:</p>
+              <p className='ml-7 text-lg'>{user?.role}</p>
+            </div>
 
-        {/* Update Button */}
-        <div className="flex items-center mt-8 justify-center">
-          <button className="flex items-center text-red-500 hover:text-red-700">
-            <FaEdit className="mr-2" />
-            <span>Update</span>
-          </button>
+            {/* Update Button */}
+            <div className='flex items-center mt-8 justify-center'>
+              <button className='flex items-center text-red-500 hover:text-red-700'>
+                <FaEdit className='mr-2' />
+                <span>Update</span>
+              </button>
+            </div>
+          </div>
         </div>
       </div>
     </div>
-  </div>
-</div>
-
   )
 }
--- src/ui/common/pages/UserDetail.tsx
+++ src/ui/common/pages/UserDetail.tsx
@@ -1,49 +1,50 @@
-import { useContext, useEffect } from "react"
-import noteContext from "context/NoteContext"
-import Posts from "../organism/Posts"
-import LeftSidebar from "../organism/LeftSidebar"
+import { useContext, useEffect, useState } from 'react'
+import noteContext from 'context/NoteContext'
+import LeftSidebar from '../organism/LeftSidebar'
 import Navbar from '@ui/common/organism/Navbar'
-import RightSidebar from "../organism/RightSidebar"
-import RightSidebarDown from "../organism/RightSidebarDown"
-import Profiles from "../organism/Profiles"
-import UserDetails from "../organism/UserDetails"
+import RightSidebar from '../organism/RightSidebar'
+import RightSidebarDown from '../organism/RightSidebarDown'
+import UserDetails from '../organism/UserDetails'
 
 const Profile = () => {
-    const a = useContext(noteContext)
-    useEffect(() => {
-        a.update()
-    }, [])
-    return (
-        <>
+  const [refreshPosts, setRefreshPosts] = useState(0)
+  const [testId, setTestId] = useState<string | null>(null)
+  const [senderDetails, setSenderDetails] = useState<any>(null)
+  const [notiService, setNotiService] = useState<any>(null)
 
-            <div className="flex ">
+  const handlePostAdded = () => {
+    setRefreshPosts((prev) => prev + 1)
+  }
 
-                {/* {a.state.name} */}
-                <div className="">
-
-                <LeftSidebar />
-                </div>
-                <div className="flex-grow">
-                    <Navbar />
-                    <div className="flex  justify-between">
-                        <div className="ml-[vh]">
-                        <UserDetails />
-                        </div>
-                        <div className="fixed right-0">
-                            <RightSidebar />
-                            
-                            <RightSidebarDown />
-                        </div>
-                    </div>
-                </div>
-              
-
-
+  const handleSetTestId = (id: string | null, senderDetails: any, notiService: any) => {
+    console.log('Test ID:', id)
+    console.log('Sender Details:', senderDetails)
+    setTestId(id)
+    setSenderDetails(senderDetails)
+    setNotiService(notiService)
+  }
+  return (
+    <>
+      <div className='flex '>
+        {/* {a.state.name} */}
+        <div className=''>
+          <LeftSidebar />
+        </div>
+        <div className='flex-grow flex flex-col'>
+          <Navbar testId={testId || ''} senderDetails={senderDetails} notiService={notiService} />
+          <div className='flex flex-grow'>
+            <div className='flex-grow flex justify-center items-center'>
+              <UserDetails />
             </div>
+            <div className=' flex flex-col '>
+              <RightSidebar setTestId={handleSetTestId} />
 
-        </>
-    )
+              <RightSidebarDown onPostAdded={handlePostAdded} />
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  )
 }
 export default Profile
-
-
