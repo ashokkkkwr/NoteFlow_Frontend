1509d7953cd067a94a9f4db011140b9098535391
Your Name <you@example.com>
2024-09-18 06:45:28 +0545

    dfqe

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,31 @@
-import React, { useContext, useEffect } from 'react';
-import { RouterProvider, createBrowserRouter } from 'react-router-dom';
-import NoteState from 'context/NoteState';
-import LandingPageTemplate from '@ui/common/templates/LandingPage.templete';
-import Signup from '@ui/common/pages/Signup';
-import Login from '@ui/common/pages/Login';
-import AuthTemplete from '@ui/common/templates/AuthTemplete';
-import FriendRequest from '@ui/common/pages/FriendRequest';
-import AddFriends from '@ui/common/pages/AddFriends';
-import ViewFriends from '@ui/common/pages/ViewFriends';
-import Landing from '@ui/common/pages/Landing';
-import Profile from '@ui/common/pages/Profile';
-import UserDetails from '@ui/common/pages/UserDetail';
-import ProtectedRoute from './ProtectedRoute';
-import ViewAllFriends from '@ui/common/pages/ViewAllFriend';
-import Settings from '@ui/common/pages/Settings';
-import ViewAllUser from '@ui/common/pages/ViewAllUser';
-import Logout from '@ui/common/molecules/Logout';
-import ChatOrganism from '@ui/common/organism/Chat.organsim';
-import userContext from '@context/User/UserContext';
-import axiosInstance from 'services/instance';
-import { io } from 'socket.io-client';
-import { SocketProvider } from '@context/SocketContext';
+import React, { useContext, useEffect } from 'react'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import NoteState from 'context/NoteState'
+import LandingPageTemplate from '@ui/common/templates/LandingPage.templete'
+import Signup from '@ui/common/pages/Signup'
+import Login from '@ui/common/pages/Login'
+import AuthTemplete from '@ui/common/templates/AuthTemplete'
+import FriendRequest from '@ui/common/pages/FriendRequest'
+import AddFriends from '@ui/common/pages/AddFriends'
+import ViewFriends from '@ui/common/pages/ViewFriends'
+import Landing from '@ui/common/pages/Landing'
+import Profile from '@ui/common/pages/Profile'
+import UserDetails from '@ui/common/pages/UserDetail'
+import ProtectedRoute from './ProtectedRoute'
+import ViewAllFriends from '@ui/common/pages/ViewAllFriend'
+import Settings from '@ui/common/pages/Settings'
+import ViewAllUser from '@ui/common/pages/ViewAllUser'
+import Logout from '@ui/common/molecules/Logout'
+import ChatOrganism from '@ui/common/organism/Chat.organsim'
+import userContext from '@context/User/UserContext'
+import axiosInstance from 'services/instance'
+import { io } from 'socket.io-client'
+import { SocketProvider } from '@context/SocketContext'
 import './index.css'
-import {SidebarProvider} from './context/SidebarContext'
-import { RightSidebarProvider } from '@context/RightSidebarContext';
-import { AutoCorrectProvider } from '@context/AutoCorrectContext';
-import AboutUs from '@ui/common/pages/AboutUs';
+import { SidebarProvider } from './context/SidebarContext'
+import { RightSidebarProvider } from '@context/RightSidebarContext'
+import { AutoCorrectProvider } from '@context/AutoCorrectContext'
+import AboutUs from '@ui/common/pages/AboutUs'
 // import NotificationComponent from './NotificationComponent';
 
 const socket = io('http://localhost:5000', {
@@ -35,24 +35,22 @@ const socket = io('http://localhost:5000', {
 })
 
 interface User {
-  id: string,
-  createdAt: any,
+  id: string
+  createdAt: any
   details: {
-    first_name: string,
-    last_name: string,
-    phone_number: string,
+    first_name: string
+    last_name: string
+    phone_number: string
     profileImage: Media[]
-  },
-  email: string,
+  }
+  email: string
   role: string
 }
 
 interface Media {
-  id: string;
-  path: string;
+  id: string
+  path: string
 }
-
-
 
 const router = createBrowserRouter([
   // Landing page
@@ -61,7 +59,7 @@ const router = createBrowserRouter([
     element: (
       <ProtectedRoute>
         <LandingPageTemplate />
-      {/* <NotificationComponent /> */}
+        {/* <NotificationComponent /> */}
       </ProtectedRoute>
     ),
     children: [{ index: true, element: <Landing /> }],
@@ -162,35 +160,33 @@ const router = createBrowserRouter([
       },
     ],
   },
-]);
+])
 
 function App() {
   const userActive = async () => {
     try {
       socket.emit('active')
-       
     } catch (error) {
       console.log(error, 'yo chai error')
     }
   }
 
-  useEffect(()=>{
+  useEffect(() => {
     userActive()
-  },[])
-  
+  }, [])
+
   return (
     <AutoCorrectProvider>
-    <RightSidebarProvider>
-    <SidebarProvider>
-    <SocketProvider>
-    <NoteState>
-      <RouterProvider router={router} />
-    </NoteState>
-    </SocketProvider>
-    </SidebarProvider>
-    </RightSidebarProvider>
+      <RightSidebarProvider>
+        <SidebarProvider>
+          <SocketProvider>
+            <NoteState>
+              <RouterProvider router={router} />
+            </NoteState>
+          </SocketProvider>
+        </SidebarProvider>
+      </RightSidebarProvider>
     </AutoCorrectProvider>
-  );
+  )
 }
-
-export default App;
+export default App
